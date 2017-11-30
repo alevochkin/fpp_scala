457aa7b69208c84cab87aff042a7c319fb266feb
alexander.levochkin <alexander.levochkin@synesis.ru>
2017-11-30 11:40:29 +0300

    remove redundant test

--- forcomp/src/test/scala/forcomp/AnagramsSuite.scala
+++ forcomp/src/test/scala/forcomp/AnagramsSuite.scala
@@ -114,25 +114,4 @@ class AnagramsSuite extends FunSuite {
     )
     assert(sentenceAnagrams(sentence).toSet === expected.toSet)
   }
-
-  test("sentence anagrams: Heather") {
-    val sentence = List("Heather")
-    val expected = List(
-      List("en", "as", "my"),
-      List("en", "my", "as"),
-      List("man", "yes"),
-      List("men", "say"),
-      List("as", "en", "my"),
-      List("as", "my", "en"),
-      List("sane", "my"),
-      List("Sean", "my"),
-      List("my", "en", "as"),
-      List("my", "as", "en"),
-      List("my", "sane"),
-      List("my", "Sean"),
-      List("say", "men"),
-      List("yes", "man")
-    )
-    assert(sentenceAnagrams(sentence).toSet === expected.toSet)
-  }
 }
