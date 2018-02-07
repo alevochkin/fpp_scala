20a73ceb838b2da30122f976d91adcf8d3c6ee24
alexander.levochkin <alexander.levochkin@synesis.ru>
2018-02-07 14:27:13 +0300

    compute value optimization

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -17,48 +17,16 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
 
   def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-
-    def parseExpr(expr: Expr): List[String] = expr match {
-      case Literal(v) => List(v.toString)
-      case Ref(n) => List(n)
-      case Plus(a, b) => parseExpr(a) ++: parseExpr(b)
-      case Minus(a, b) => parseExpr(a) ++: parseExpr(b)
-      case Times(a, b) => parseExpr(a) ++: parseExpr(b)
-      case Divide(a, b) => parseExpr(a) ++: parseExpr(b)
-      case _ => List()
-    }
-
-    val dependencyMap: Map[String, List[String]] = namedExpressions.map { case (key, value) => (key, parseExpr(value())) }
-
-    def getValue(key: String) = {
-      val expr = namedExpressions(key)
-      if (check(key, dependencyMap(key), dependencyMap, false)) {
-        Signal(Double.NaN)
-      } else {
-        Signal(eval(expr(), namedExpressions))
-      }
-    }
-
-    def check(name: String, keys: List[String], map: Map[String, List[String]], result: Boolean ): Boolean = {
-      if(keys.isEmpty || result) {
-        result
-      } else {
-        check(name, keys flatMap { key =>  map get key} flatten, map, keys.contains(name) || result)
-      }
-    }
-
-    namedExpressions.foldLeft(Map.empty[String, Signal[Double]]) {
-      case (map, (key, value)) => map + (key -> getValue(key))
-    }
+    namedExpressions.map{ case (key, value) => key -> Signal(eval(value(), namedExpressions - key)) }
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = expr match {
     case Literal(v) => v
-    case Ref(name) => eval(getReferenceExpr(name, references), references)
     case Plus(a, b) => eval(a, references) + eval(b, references)
     case Minus(a, b) => eval(a, references) - eval(b, references)
     case Times(a, b) => eval(a, references) * eval(b, references)
     case Divide(a, b) => eval(a, references) / eval(b, references)
+    case Ref(name) => eval(getReferenceExpr(name, references), references)
     case _ => Double.NaN
   }
 
